d7497965050e6559da71975e2527b2bd6106d3dc
DaniloTorquatoUbaldine <danilo.torquato@dbccompany.com.br>
2023-02-07 13:59:05 -0300

    Correções da lista 2

--- modulo1/lista2/src/Questao4.java
+++ modulo1/lista2/src/Questao4.java
@@ -5,23 +5,30 @@ public class Questao4 {
         Scanner leitorDoUsuario = new Scanner(System.in);
         double[][] matriz = new double[5][4];
         for (int i = 0;  i < 5; i = i + 1){
-            for (int y = 0;  y < 3; y = y + 1){
-                System.out.println("Digite as informações na ordem: número da matrícula, média da prova, média do trabalho. para cada aluno.");
-                double valor = leitorDoUsuario.nextDouble();
-                leitorDoUsuario.nextLine();
-                matriz[i][y] = valor;
+            for (int y = 0;  y < 4; y = y + 1){
+                if (y == 3){
+                    matriz[i][3] = (matriz[i][1] * 0.6) + (matriz[i][2] * 0.4);
+                } else {
+                    System.out.println("Digite as informações na ordem: número da matrícula, média da prova, média do trabalho. para cada aluno.");
+                    double valor = leitorDoUsuario.nextDouble();
+                    leitorDoUsuario.nextLine();
+                    matriz[i][y] = valor;
+                }
+
+
             }
         }
-        matriz[0][3] = (matriz[0][0] * 0.6) + (matriz[0][2] * 0.4);
-        matriz[1][3] = (matriz[1][0] * 0.6) + (matriz[1][2] * 0.4);
-        matriz[2][3] = (matriz[2][0] * 0.6) + (matriz[2][2] * 0.4);
-        matriz[3][3] = (matriz[3][0] * 0.6) + (matriz[3][2] * 0.4);
-        matriz[4][3] = (matriz[4][0] * 0.6) + (matriz[4][2] * 0.4);
+        //matriz[0][3] = (matriz[0][1] * 0.6) + (matriz[0][2] * 0.4);
+        //matriz[1][3] = (matriz[1][1] * 0.6) + (matriz[1][2] * 0.4);
+        //matriz[2][3] = (matriz[2][1] * 0.6) + (matriz[2][2] * 0.4);
+        //matriz[3][3] = (matriz[3][1] * 0.6) + (matriz[3][2] * 0.4);
+        //matriz[4][3] = (matriz[4][1] * 0.6) + (matriz[4][2] * 0.4);
         int y  = 3;
         double maiorNota = 0;
         int matricula  = -1;
         double media = 0;
         double soma = 0;
+
         for (int i = 0; i < 5; i = i + 1){
             if (matriz[i][y] > maiorNota){
                 maiorNota = matriz[i][y];
@@ -30,6 +37,6 @@ public class Questao4 {
             soma = soma + matriz[i][y];
         }
         media = soma / 5;
-        System.out.println("A matrícula que obteve a maior nota foi " + matriz[matricula][0] + ". A média das notas finais é " + media);
+        System.out.println("A matrícula que obteve a maior nota foi " + matriz[matricula][0] + ". A média das notas finais é " + media + ".");
     }
 }
--- modulo1/lista2/src/Questao5.java
+++ modulo1/lista2/src/Questao5.java
@@ -2,36 +2,50 @@ import java.util.Scanner;
 
 public class Questao5 {
     public static void main(String[] args) {
+        double menorPreco = 9999999;
+        double media1 = 0;
+        double soma1 = 0;
+        double media2 = 0;
+        double soma2 = 0;
+        double media3 = 0;
+        double soma3 = 0;
         Scanner leitorDoUsuario = new Scanner(System.in);
         double[][] matriz = new double[10][3];
         for (int i = 0;  i < 10; i = i + 1){
             for (int y = 0;  y < 3; y = y + 1){
-                System.out.println("Digite o preço de cada produto no primeiro mercado, depois no segundo mercado e depois no terceiro mercado");
+                System.out.println("Digite o preço de cada produto no primeiro mercado, depois no segundo mercado e depois no terceiro mercado.");
                 double valor = leitorDoUsuario.nextDouble();
                 leitorDoUsuario.nextLine();
                 matriz[i][y] = valor;
+                if (y == 0){
+                    soma1 = soma1 + matriz[i][0];
+                }
+                if (y == 1){
+                    soma2 = soma2 + matriz[i][1];
+                }
+                if (y == 2){
+                    soma3 = soma3 + matriz[i][2];
+                }
+
             }
         }
-        int y = 0;
-        double media1 = 0;
-        double soma1 = 0;
-        for (int i = 0; i < 10; i = i + 1){
-            soma1 = soma1 + matriz[i][y];
-        }
-        media1 = soma1 / 10;
-        y = 1;
-        double media2 = 0;
-        double soma2 = 0;
-        for (int i = 0; i < 10; i = i + 1){
-            soma2 = soma2 + matriz[i][y];
-        }
-        media2 = soma2 / 10;
-        y = 2;
-        double media3 = 0;
-        double soma3 = 0;
-        for (int i = 0; i < 10; i = i + 1){
-            soma3 = soma3 + matriz[i][y];
-        }
+        //int y = 0;
+
+        //for (int i = 0; i < 10; i = i + 1){
+           // soma1 = soma1 + matriz[i][y];
+       // }
+        //media1 = soma1 / 10;
+       // y = 1;
+
+       // for (int i = 0; i < 10; i = i + 1){
+         //   soma2 = soma2 + matriz[i][y];
+       // }
+       // media2 = soma2 / 10;
+       // y = 2;
+
+       // for (int i = 0; i < 10; i = i + 1){
+         //   soma3 = soma3 + matriz[i][y];
+       // }
 
 
         double[] vetor = new double[10];
@@ -39,7 +53,9 @@ public class Questao5 {
         vetor[1] = soma2;
         vetor[2] = soma3;
 
-        double menorPreco = 9999999;
+        System.out.println(soma1);
+        System.out.println(soma2);
+        System.out.println(soma3);
         int mercado = 0;
 
         if (soma1 < soma2 && soma1 < soma3){
